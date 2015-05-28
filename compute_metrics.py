c00b1ccb1715f8f6d1b2cfcbb4d96e9fd25a6cea
Andrej Gajduk <gajduk.andrej@gmail.com>
2015-05-28 15:28:41 +0200

    Initial commit

---
+++ compute_metrics.py
@@ -0,0 +1,206 @@
+import os,sys,random
+import networkx as nx
+from load_data import *
+       
+def calcTransTrip(g):
+    res = {}
+    avg_c = 0
+    for i in range(g.number_of_nodes()):
+        c = 0
+        for j in range(g.number_of_nodes()):
+            if g.has_edge(i,j):
+                for h in range(g.number_of_nodes()):
+                    if g.has_edge(i,h) and g.has_edge(j,h):
+                        c += 1
+        avg_c += c                
+        res[i] = c
+    return res,avg_c*1.0/g.number_of_nodes()
+    
+def calcThreeCycles(g):
+    res = {}
+    avg_c = 0
+    for i in range(g.number_of_nodes()):
+        c = 0
+        for j in range(g.number_of_nodes()):
+            if g.has_edge(i,j):
+                for h in range(g.number_of_nodes()):
+                    if g.has_edge(h,i) and g.has_edge(j,h):
+                        c += 1
+        avg_c += c                
+        res[i] = c
+    return res,avg_c*1.0/g.number_of_nodes() 
+    
+def calcTransTies(g):
+    res = {}
+    avg_c = 0
+    for i in range(g.number_of_nodes()):
+        c = 0
+        for j in range(g.number_of_nodes()):
+            if g.has_edge(i,j):
+                for h in range(g.number_of_nodes()):
+                    if g.has_edge(i,h) and g.has_edge(h,j):
+                        c += 1
+                        break
+        avg_c += c                
+        res[i] = c
+    return res,avg_c*1.0/g.number_of_nodes() 
+
+def calcBalance(g):
+    res = {}
+    avg_c = 0
+    for i in range(g.number_of_nodes()):
+        c = 0
+        b = 0
+        for j in range(g.number_of_nodes()):
+            if g.has_edge(i,j):
+                for h in range(g.number_of_nodes()):
+                    if h == j or h == i:
+                        continue
+                    if g.has_edge(i,h) and not g.has_edge(j,h):
+                        b += 1
+                    else:
+                        if not g.has_edge(i,h) and g.has_edge(j,h):
+                            b += 1
+        b = b*1.0/((g.number_of_nodes()-2)*(g.number_of_nodes()-1))
+        
+        has_at_least_one_out_edge = False
+        for j in range(g.number_of_nodes()):
+            v = 0
+            if g.has_edge(i,j):      
+                has_at_least_one_out_edge = True
+                for h in range(g.number_of_nodes()):
+                    if h == j or h == i:
+                        continue
+                    if g.has_edge(i,h) and not g.has_edge(j,h):
+                        v += b-1
+                    else:
+                        if not g.has_edge(i,h) and g.has_edge(j,h):
+                            v += b-1
+                        else:
+                            v += b
+            c += v
+        if not has_at_least_one_out_edge:
+            c = g.number_of_nodes()-2             
+        avg_c += c*-1.0/(g.number_of_nodes()-2)                
+        res[i] = c*-1.0/(g.number_of_nodes()-2)     
+    return res,avg_c*1.0/g.number_of_nodes() 
+   
+def calcPopIn(g):
+    res = {}
+    avg_c = 0
+    for i in range(g.number_of_nodes()):
+        c_i = 0
+        for j in range(g.number_of_nodes()):
+            c = 0
+            if g.has_edge(i,j):
+                for k in range(g.number_of_nodes()):
+                    if g.has_edge(k,j):
+                        c += 1
+            c = c**.5
+            c_i += c
+        avg_c += c_i               
+        res[i] = c_i
+    return res,avg_c*1.0/g.number_of_nodes() 
+    
+def calcPopOut(g):
+    res = {}
+    avg_c = 0
+    for i in range(g.number_of_nodes()):
+        c_i = 0
+        for j in range(g.number_of_nodes()):
+            c = 0
+            if g.has_edge(i,j):
+                for k in range(g.number_of_nodes()):
+                    if g.has_edge(j,k):
+                        c += 1
+            c = c**.5
+            c_i += c
+        avg_c += c_i               
+        res[i] = c_i
+    return res,avg_c*1.0/g.number_of_nodes() 
+
+def calcActIn(g):
+    res = {}
+    avg_c = 0
+    for i in range(g.number_of_nodes()):
+        c_i = 0
+        for j in range(g.number_of_nodes()):
+            c = 0
+            if g.has_edge(j,i):
+                for k in range(g.number_of_nodes()):
+                    if g.has_edge(i,k):
+                        c += 1
+            c = c**.5
+            c_i += c
+        avg_c += c_i               
+        res[i] = c_i
+    return res,avg_c*1.0/g.number_of_nodes() 
+    
+def calcActOut(g):
+    res = {}
+    avg_c = 0
+    for i in range(g.number_of_nodes()):
+        c_i = 0
+        for j in range(g.number_of_nodes()):
+            c = 0
+            if g.has_edge(i,j):
+                for k in range(g.number_of_nodes()):
+                    if g.has_edge(i,k):
+                        c += 1
+            c = c**.5
+            c_i += c
+        avg_c += c_i               
+        res[i] = c_i
+    return res,avg_c*1.0/g.number_of_nodes() 
+
+metrics_dict = {"Transitivity Triplets":calcTransTrip,"Three Cycles":calcThreeCycles,"Balance":calcBalance,\
+                "Popularity Ingoing":calcPopIn,"Popularity Outgoing":calcPopOut,"Activity Ingoing":calcActIn,"Activity Outgoing":calcActOut}
+
+def getMetric(g,metric="Trans Trip"):
+    if metric in metrics_dict:
+        return metrics_dict[metric](g)
+    else:
+        print "ERROR metric:"+metric+" not implemented"
+        return None
+
+def addMetricsAsAttributes(graphs,metrics="all"):
+    if metrics == "all":
+        metrics = []
+        for key in metrics_dict:
+            metrics.append(key)
+    for metric in metrics:
+        if metric in metrics_dict:
+            for g in graphs:
+                for_node,avg = getMetric(g,metric)
+                g.graph[metric] = [for_node[key] for key in range(g.number_of_nodes())]
+    return graphs
+
+def scatterPlotForMetrics(g1,g2,attribute):
+    x = g1.graph[attribute]
+    y = g2.graph[attribute]
+    plt.scatter(x,y)
+    plt.plot([min(x+y),max(x+y)],[min(x+y),max(x+y)],'k-',lw=2)
+    plt.xlabel(g1.graph["title"])
+    plt.ylabel(g2.graph["title"])
+    plt.xlim(min(x+y),max(x+y))
+    plt.ylim(min(x+y),max(x+y))
+    plt.title(attribute)
+ 
+def main():
+    # g = getTestGraph1()
+    # print ["%.2f" %value for key,value in calcPopIn(g)[0].iteritems()]
+    # print ["%.2f" %value for key,value in calcPopOut(g)[0].iteritems()]
+    # print ["%.2f" %value for key,value in calcActOut(g)[0].iteritems()]
+    # print ["%.2f" %value for key,value in calcActIn(g)[0].iteritems()]
+    graphs,node_mapping = load_4_layers()
+    graphs = addMetricsAsAttributes(graphs)
+    i = 1
+    for metric in metrics_dict:
+        plt.subplot(3,3,i)
+        scatterPlotForMetrics(graphs[2],graphs[5],metric)
+        i += 1
+    plt.show()
+    
+if __name__ == "__main__"    :
+    main()
+    
---
+++ load_data.py
@@ -0,0 +1,90 @@
+import os,sys,random
+import matplotlib.pyplot as plt
+import networkx as nx
+
+def load_4_layers(dataset="wan",aggregated=False):
+    path = "data\\wan.csv"
+    if dataset == "bms":
+        path = "data\\bms.csv"
+    node_mapping = {}
+    count = 0
+    graphs = [nx.DiGraph(),nx.DiGraph(),nx.DiGraph(),nx.DiGraph(),nx.DiGraph(),nx.DiGraph(),nx.DiGraph()]
+    descr = ["strong real","weak real","strong facebook","weak facebook","real","facebook","aggregated"]
+    with open(path,"r") as f:
+        for line in f:
+            s_line = line.split(",")
+            from_node = int(s_line[0])
+            to_node = int(s_line[2])
+            layer = int(s_line[1])
+            count = _addNode(from_node,node_mapping,count,graphs)
+            count = _addNode(to_node,node_mapping,count,graphs)
+            graphs[layer-1].add_edge(node_mapping[from_node],node_mapping[to_node])
+            if layer < 3:
+                graphs[4].add_edge(node_mapping[from_node],node_mapping[to_node])
+            else:
+                graphs[5].add_edge(node_mapping[from_node],node_mapping[to_node])
+            graphs[6].add_edge(node_mapping[from_node],node_mapping[to_node])
+    for i in range(len(graphs)):
+        graphs[i].graph["title"] = dataset+" "+descr[i]
+    res = graphs
+    graphs = [res[0],res[1],res[4],res[2],res[3],res[5],res[6]]
+    return (graphs,node_mapping)
+    
+def _addNode(node,node_mapping,count,graphs):
+    if not node in node_mapping:
+        node_mapping[node] = count
+        for graph in graphs:
+            graph.add_node(count)
+        count += 1
+    return count
+    
+def getTestGraph1():
+    g = nx.DiGraph()
+    for i in range(14):
+        g.add_node(i)
+    edges = [(0,1),(0,2),(2,1),(3,4),(4,5),(5,3),(6,7),(7,8),(6,8),(6,9),(9,8),(10,11),(11,10),(10,12),(12,11),(3,13),(13,5)]
+    for i,j in edges:
+        g.add_edge(i,j)
+    return g
+    
+def getTestGraph2():
+    g = nx.DiGraph()
+    for i in range(8):
+        g.add_node(i)
+    edges = [(0,1),(0,2),(0,3),(1,3),(3,1),(2,1),(2,3),(3,2),(4,5),(4,6),(4,7),(7,6)]
+    for i,j in edges:
+        g.add_edge(i,j)
+    return g
+       
+    
+def drawGraphs(graphs,color_attribute=None,aggregated=False):
+    if not color_attribute is None:
+        plt.suptitle(color_attribute)
+    pos = nx.spring_layout(graphs[6],iterations=7,k=.3)
+    i = 1
+    for graph in graphs:
+        if aggregated:
+            plt.subplot(3,3,i)
+        else:
+            plt.subplot(2,3,i)            
+        if color_attribute == None:
+            nx.draw(graph,pos,node_size=10,edgelist=[])
+        else:
+            nx.draw(graph,pos,node_size=[v+10 for v in graph.graph[color_attribute]],edgelist=[],node_color=graph.graph[color_attribute])
+        nx.draw_networkx_edges(graph,pos,alpha=0.1,arrows=False,edge_color =[[.6,.6,.6]]*graph.number_of_edges())
+        #nx.draw_networkx_labels(graph,pos,{i:str(i) for i in range(graph.number_of_nodes())},font_size=16)
+        plt.title(graph.graph["title"])
+        i += 1
+        if not aggregated and i > 6:
+            break
+    plt.show()
+        
+
+
+def main():
+    graphs,node_mapping = load_4_layers()
+    drawGraphs(graphs)
+    
+if __name__ == "__main__":
+    main()
+    
